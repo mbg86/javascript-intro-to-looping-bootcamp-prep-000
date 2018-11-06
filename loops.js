efb291f81b7766c808c099ec2e44db15de539006
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-11-06 00:15:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,7 @@ function forLoop(array) {
 
 function whileLoop(array) {
   let (countdown = 100)
-  while(countdown > 0)
+  while (countdown > 0) {
+  console.log(--countdown);}
   return 'done'
 }
